57dda2358e488faec77d02ee63a152b09d606e40
giopal04 <g.palmieri4998@gmail.com>
2026-01-29 15:56:01 +0000

    Aggiornato Image-tensor_transforms.py

--- Image_tensor_transforms.py
+++ Image_tensor_transforms.py
@@ -3,9 +3,13 @@ import numpy as np
 from PIL import Image
 from torchvision.transforms.v2 import ToPILImage
 
-def PIL2Tensor(image: Image) -> torch.Tensor:
+def PIL2Tensor(image: Image, is_img: bool = True) -> torch.Tensor:
 
-    return torch.tensor(np.array(image))
+    if is_img:
+        return torch.tensor(np.array(image)).permute(2,0,1)
+    
+    else:
+        return torch.tensor(np.array(image))
 
 def Tensor2PIL(tensor: torch.Tensor) -> Image:
     to_image = ToPILImage()
